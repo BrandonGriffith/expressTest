d6d392ad5c9b6c923fdbcb3d87e36032326a62b6
BrandonGriffith <BrandonGriffith92@gmail.com>
2025-08-05 22:08:29 -0400

    add color to logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,12 @@
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol} ${req.get('host')} ${req.originalUrl}`);
+    const methodColor = '\x1b[32m'; // Green
+    const protocolColor = '\x1b[34m'; // Blue
+    const hostColor = '\x1b[36m'; // Cyan
+    const urlColor = '\x1b[33m'; // Yellow
+    const resetColor = '\x1b[0m'; // Reset
+
+    console.log(`${methodColor}${req.method} ${protocolColor}${req.protocol} ${hostColor}${req.get('host')} ${urlColor}${req.originalUrl} ${resetColor}`);
     next();
 };
+
 export default logger;
